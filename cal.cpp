b662aa76eba6be682c0f66182ab8b4fec2f851a9
Shubham Keluskar <124715970+Shubhamkeluskar@users.noreply.github.com>
2023-11-05 00:39:46 +0530

    Add files via upload

---
+++ cal.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Simple Calculator Program" << endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    double result = 0.0;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+                return 1;  // Exit with an error code
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please use +, -, *, or /." << endl;
+            return 1;  // Exit with an error code
+    }
+
+    cout << "Result: " << result << endl;
+    return 0;
+}
